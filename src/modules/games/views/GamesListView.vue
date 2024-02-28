56d046c0168cadb4e0930e25dcae675b4ad120c5
Missael Padilla <lmpadillar@gmail.com>
2024-02-27 21:26:14 -0800

    Fix design

--- src/modules/games/views/GamesListView.vue
+++ src/modules/games/views/GamesListView.vue
@@ -11,19 +11,19 @@ const { games, isLoading, count, wons, loss } = useGames();
 
 <template>
   <Loading  v-if="isLoading" />
-  <div class="lg:flex lg:items-center lg:justify-between p-3"  v-else>
+  <div class="lg:flex lg:flex-col items-center justify-between p-3"  v-else>
     <div class="min-w-0 flex-1">
       <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Juegos Jugados - {{ count }}</h2>
       <div class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
         <div class="mt-2 flex items-center text-sm text-gray-500 font-bold text-2xl">
-          <i class="fas fa-check"></i>
+          <i class="fas fa-check mr-1"></i>
           {{ wons }}
         </div>
         <div class="mt-2 flex items-center text-sm text-gray-500 font-bold text-2xl">
-          <i class="fas fa-times"></i>
+          <i class="fas fa-times mr-1"></i>
           {{ loss }}
         </div>
-        <div class="mt-2 flex items-center text-sm text-gray-500">
+        <div class="mt-2 flex items-center text-sm text-gray-500 justify-end">
             <a class="btn-primary mt-2">
                 <span class="mr-2">Agregar Juego</span>
                 <i class="fa-regular fa-add"></i>
@@ -38,7 +38,7 @@ const { games, isLoading, count, wons, loss } = useGames();
             <th class="table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">VS</th>
             <th class="hidden md:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Carreras Favor</th>
             <th class="hidden md:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Carreras Contra</th>
-            <th class="md:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
+            <th class="md:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Resultado</th>
           </tr>
         </thead>
         <tbody>
